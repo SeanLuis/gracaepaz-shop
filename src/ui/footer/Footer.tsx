b1963666795ff24085af186f6cac29fd88e3c36a
Sean Luis <seanluis47@gmail.com>
2024-08-30 22:29:07 -0300

    fix: translations on footer categories

--- src/ui/footer/Footer.tsx
+++ src/ui/footer/Footer.tsx
@@ -1,11 +1,11 @@
 import { type SVGAttributes } from "react";
 import { getTranslations } from "next-intl/server";
-import { Categories } from "@/ui/nav/Nav";
 import { YnsLink } from "@/ui/YnsLink";
 import { Newsletter } from "@/ui/footer/Newsletter.client";
 
 export async function Footer() {
 	const t = await getTranslations("Global");
+
 	return (
 		<footer className="w-full bg-neutral-50 p-6 text-neutral-800 md:py-12">
 			<div className="container flex max-w-7xl flex-row flex-wrap justify-center gap-16 text-sm sm:justify-between">
@@ -14,21 +14,8 @@ export async function Footer() {
 					<Newsletter />
 				</div>
 				<nav className="grid grid-cols-2 gap-16">
-					<section>
-						<h3 className="mb-2 font-semibold">{t("footer.categoriesTitle")}</h3>
-						<ul role="list" className="grid gap-1">
-							{Categories.map((category) => (
-								<li key={category.slug}>
-									<YnsLink
-										className="underline-offset-4 hover:underline"
-										href={`/category/${category.slug}`}
-									>
-										{t(`navbar.Shop.items.${category.key}.title`)}
-									</YnsLink>
-								</li>
-							))}
-						</ul>
-					</section>
+					<CategoryLink title={t("navbar.Shop.items.bags.title")} slug="bags" />
+					<CategoryLink title={t("navbar.Shop.items.backpacks.title")} slug="backpacks" />
 					<section>
 						<h3 className="mb-2 font-semibold">Misc</h3>
 						<ul role="list" className="grid gap-1">
@@ -54,7 +41,7 @@ export async function Footer() {
 						className="inline-flex items-center gap-1 transition-colors hover:text-neutral-700"
 						href="https://www.instagram.com/graca_e_paz_croche"
 					>
-						<TwitterIcon className="h-4 w-4" /> @gracaepazcroche
+						<InstagramIcon className="h-4 w-4" /> @gracaepazcroche
 						<span className="sr-only">Instagram</span>
 					</YnsLink>
 				</div>
@@ -63,7 +50,27 @@ export async function Footer() {
 	);
 }
 
-function TwitterIcon(props: SVGAttributes<SVGSVGElement>) {
+type CategoryLinkProps = {
+	title: string;
+	slug: string;
+};
+
+function CategoryLink({ title, slug }: CategoryLinkProps) {
+	return (
+		<section>
+			<h3 className="mb-2 font-semibold">{title}</h3>
+			<ul role="list" className="grid gap-1">
+				<li>
+					<YnsLink className="underline-offset-4 hover:underline" href={`/category/${slug}`}>
+						{title}
+					</YnsLink>
+				</li>
+			</ul>
+		</section>
+	);
+}
+
+function InstagramIcon(props: SVGAttributes<SVGSVGElement>) {
 	return (
 		<svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 596 596" fill="none">
 			<path
